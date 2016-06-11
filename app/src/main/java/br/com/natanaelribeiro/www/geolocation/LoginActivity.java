736ee51a90e0494f86b2fe69bd88af8437da232d
“Natanael <natanaelribeiro17@gmail.com>
2016-06-11 14:52:08 -0300

    Logins + maps

--- app/src/main/java/br/com/natanaelribeiro/www/geolocation/LoginActivity.java
+++ app/src/main/java/br/com/natanaelribeiro/www/geolocation/LoginActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private GoogleApiClient mGoogleApiClient;
+    private final int RC_SIGN_IN = 1;
     private CallbackManager callbackManager;
 
     @Override
@@ -21,34 +30,84 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+
+
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
-        if (loginButton != null) {
-            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        if(AccessToken.getCurrentAccessToken() == null) {
 
-                @Override
-                public void onSuccess(LoginResult loginResult) {
-                    Log.e("CURSO", "toString: " + loginResult.toString());
-                    Log.e("CURSO", "accesstoken: " + loginResult.getAccessToken());
-                    Log.e("CURSO", "token: " + loginResult.getAccessToken().getToken());
-                    Log.e("CURSO", "granted: " + loginResult.getRecentlyGrantedPermissions());
-                    Log.e("CURSO", "denied: " + loginResult.getRecentlyDeniedPermissions());
-                }
+            LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
-                @Override
-                public void onCancel() {}
+            if (loginButton != null) {
+                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
-                @Override
-                public void onError(FacebookException error) {}
-            });
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.e("CURSO", "toString: " + loginResult.toString());
+                        Log.e("CURSO", "accesstoken: " + loginResult.getAccessToken());
+                        Log.e("CURSO", "token: " + loginResult.getAccessToken().getToken());
+                        Log.e("CURSO", "granted: " + loginResult.getRecentlyGrantedPermissions());
+                        Log.e("CURSO", "denied: " + loginResult.getRecentlyDeniedPermissions());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                    }
+                });
+            }
+        } else {
+            proximaTela();
         }
 
     }
 
+    public void proximaTela(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.d("CURSO", "displayname: " + acct.getDisplayName());
+            Log.d("CURSO", "displayname: " + acct.getEmail());
+            proximaTela();
+        } else {
+            Log.d("CURSO", "sign out");
+        }
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+
     }
 }
